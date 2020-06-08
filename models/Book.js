ed0ab941c0deccb7f837a39ec304082d34a6fa0d
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-06-08 18:20:32 +0700

    Updated Default Book Cover & Avatar

--- models/Book.js
+++ models/Book.js
@@ -1,23 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   description: {
     type: String,
-    required: true
+    required: true,
   },
   coverUrl: {
     type: String,
-    default: "/uploads/books/371f645d721a5be1f722fa80f22b5fc8"
+    default:
+      'https://res.cloudinary.com/dbu9wn1oz/image/upload/v1591615171/BookManagement/Books/book-cover-default.png',
   },
   date: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model("Book", bookSchema);
+module.exports = mongoose.model('Book', bookSchema);
--- models/User.js
+++ models/User.js
@@ -1,36 +1,37 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   phone: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
     unique: true,
-    required: true
+    required: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   isAdmin: {
     type: Boolean,
-    default: false
+    default: false,
   },
   wrongLoginCount: {
     type: Number,
-    default: 0
+    default: 0,
   },
   avatarUrl: {
     type: String,
-    default: "/uploads/avatars/f648dfdd0b3a85231de874e91f694070"
-  }
+    default:
+      'https://res.cloudinary.com/dbu9wn1oz/image/upload/v1587877777/BookManagement/Avatars/f648dfdd0b3a85231de874e91f694070.png',
+  },
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
